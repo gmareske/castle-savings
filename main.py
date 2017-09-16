2531b7ad7f1f9da75388580d53b1ace83e6f0e30
Philip Kiely <Philip@Philips-MacBook-Air.local>
2017-09-16 11:37:47 -0500

    web basics

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, render_template
 
 app = Flask(__name__)
 
 @app.route("/",methods=['GET'])
 def hello():
     """ respond to basic web request """
-    return "Hello World!"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
