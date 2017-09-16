86cec75cd0acd75781d7f0acb2bb7a280b70a5a8
Griffin Mareske <gmareske@gmail.com>
2017-09-16 11:30:30 -0500

    Add models for data

---
+++ goal.py
@@ -0,0 +1,7 @@
+class Goal(object):
+
+    def __init__(self, name, balance=0):
+        self.name = name
+        self.balance = balance
+        self.keywords = [name]
+        
---
+++ user.py
@@ -0,0 +1,18 @@
+class User(object):
+    
+    def __init__(self, name, number):
+        self.goals = []
+        self.messages = []
+        self.name = name
+        self.number = number
+        
+    def add_msg(self, from_who, msg):
+        """
+        Add a message to the message history
+        """
+        pass
+
+    def add_goal(self, goal):
+        self.goals.append(goal)
+
+    
